64794564bab59e0751f0161c042bbcbfc2b9ff65
R1\korisnik <daria.alilovic@gmail.com>
2017-10-16 23:43:31 +0200

    Prazan projekt

---
+++ head.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>       
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="darson.css">
+    </head>
+    <body>      
+<nav class="navbar navbar-ineverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Darson</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Početna</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">O nama <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Mediji</a></li>
+      <li><a href="#">Galerija</a></li>
+      <li><a href="#">Kontakt</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="registracija.php"><span class="glyphicon glyphicon-user"></span> Registriraj se</a></li>
+      <li><a href="prijava.php"><span class="glyphicon glyphicon-log-in"></span> Prijavi se</a></li>
+    </ul>
+  </div>
+</nav>
